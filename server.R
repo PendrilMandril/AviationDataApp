d18aff83db88f93b2c3d467abe9549afc0017ace
Pendril Mandril <PendrilMandril@github.com>
2017-02-04 13:23:15 +0100

    uploading app

---
+++ server.R
@@ -0,0 +1,49 @@
+library(ggplot2)
+library(dplyr)
+library(magrittr)
+library(lubridate)
+
+flights <- readr::read_csv("AviationDataEnd2016UP.csv")
+flights$Year <- year(flights$Event.Date) %>% as.integer()
+flights <- select(flights, Total.Fatal.Injuries, Year, Purpose.of.Flight,
+                  Aircraft.Damage, Report.Status, Country, Aircraft.Category, Amateur.Built) 
+
+flights$Amateur.Built  %<>%  as.factor()
+function(input, output) {
+  
+  # Filter the flights, returning a data frame
+  getFlights <- reactive({
+    f <- flights %>% filter(
+      Total.Fatal.Injuries > input$fatalInjuries, 
+      Year > input$year[1],
+      Year < input$year[2]
+    )
+    
+    if (input$purposeFlight != "All") {
+      f <- f %>% filter(Purpose.of.Flight == input$purposeFlight)
+    }
+    if (input$aircraftDamage != "All" & input$aircraftDamage != "") {
+      f <- f %>% filter(Aircraft.Damage == input$aircraftDamage)
+    }
+    if (input$reportStatus != "All" & input$reportStatus != "") {
+      f <- f %>% filter(Report.Status == input$reportStatus)
+    }
+    if (input$country!= "All" & input$country != "") {
+      f <- f %>% filter(Country == input$country)
+    }
+    if (input$aircraftCategory != "All" & input$aircraftCategory != "") {
+      f <- f %>% filter(Aircraft.Category == input$aircraftCategory)
+    }
+    
+    return(f)
+  })
+  
+  output$plot1 <- renderPlot({
+    myFlights <- getFlights()
+    ggplot(myFlights, aes(x = Year)) + geom_bar(aes(fill = Amateur.Built), position = "stack")
+  })
+  output$n_flights <- renderText({
+    myFlights <- getFlights()
+    nrow(myFlights) 
+  })
+}
---
+++ ui.R
@@ -0,0 +1,47 @@
+library(ggvis)
+
+# # For dropdown menu
+# actionLink <- function(inputId, ...) {
+#   tags$a(href='javascript:void',
+#          id=inputId,
+#          class='action-button',
+#          ...)
+# }
+
+fluidPage(
+  titlePanel("Aviation data explorer"),
+  fluidRow(
+    column(3,
+           wellPanel(
+             h4("Filter"),
+             sliderInput("fatalInjuries", "At least X deaths",
+                         0, 350, 0, step = 1),
+             sliderInput("year", "Year of the accident", 1982, 2017, value = c(1982, 2017)),
+             textInput("country", "Country name"),
+             selectInput("aircraftDamage", "Damage of the aircraft",
+                         c("All", "Destroyed", "Substantial", "Minor")),
+             selectInput("aircraftCategory", "Category of the aircraft",
+                         c("All", "Airplane", "Balloon", "Blimp", "Glider", "Gyrocraft",
+                           "Gyroplane", "Helicopter", "Powered-Lift", "Powered Parachute",
+                           "Rocket", "Ultralight", "Unknown", "Weight-Shift")),
+             selectInput("reportStatus", "Status of the report",
+                         c("All", "Factual", "Foreign", "Preliminary", "Probable Cause")),
+             selectInput("purposeFlight", "Purpose of the flight",
+                         c("All", "Aerial Application", "Aerial Observation", "Air Drop", "Air Race/Show",
+                           "Banner Tow", "Business", "Executive/Corporate", "External Load", "Ferry",
+                           "Firefighting", "Flight Test", "Glider Tow", "Instructional",
+                           "Other Work Use", "Personal", "Positioning", "Public Aircraft",
+                           "Public Aircraft - Federal", "Public Aircraft - Local",
+                           "Public Aircraft - State", "Skydiving", "Unknown"))
+           )
+    ),
+    column(9,
+           plotOutput("plot1"),
+           wellPanel(
+             span("Number of flights selected:",
+                  textOutput("n_flights")
+             )
+           )
+    )
+  )
+)
